1792e04e0b53ff53d0e9792d86d3e6b29e467de3
Dyalpa <117543340+Dyalpa@users.noreply.github.com>
2025-01-27 18:17:45 -0500

    navegación de las paginas de consulta de alimentos, medicamentos, citas y novedades

--- src/app/consultar-novedades/consultar-novedades.page.ts
+++ src/app/consultar-novedades/consultar-novedades.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-consultar-novedades',
@@ -12,9 +13,13 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
 })
 export class ConsultarNovedadesPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigateTo(page: string){
+    this.router.navigateByUrl(`/${page}`);
   }
 
 }
